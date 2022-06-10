e92fce38842683526748f73cd4a35c4de38e0afa
gregmccoy <greg@gkmc.ca>
2022-06-10 10:03:20 -0400

    Fixing namefolder creation for enable

--- api/api_auth.py
+++ api/api_auth.py
@@ -3,12 +3,13 @@ from flask import request
 import requests
 from api import module_api
 from models.api_meta_class import MetaAPI
-from common import LoggerFactory
+from common import LoggerFactory, ProjectClientSync
 from services.notifier_services.email_service import SrvEmail
 from flask_jwt import jwt_required, current_identity
 from config import ConfigClass
 import jwt as pyjwt
-from datetime import datetime
+from resources.error_handler import APIException
+from models.api_response import EAPIResponseCode
 
 # init logger
 _logger = LoggerFactory('api_auth_service').get_logger()
@@ -77,7 +78,7 @@ class APIAuthService(metaclass=MetaAPI):
             try:
                 token = request.headers.get('Authorization')
                 token = token.split()[-1]
-                decoded = pyjwt.decode(token, verify=False)
+                pyjwt.decode(token, verify=False)
             except Exception as e:
                 return {'result': "JWT user status error " + str(e)}, 500
             try:
@@ -94,7 +95,7 @@ class APIAuthService(metaclass=MetaAPI):
                 if not user_email and not user_id:
                     return {'result': 'either user_email or user_id required.'}, 400
                 # check user operation type
-                if not operation_type in ['enable', 'disable']:
+                if operation_type not in ['enable', 'disable']:
                     return {'result': 'operation {} is not allowed'.format(operation_type)}, 400
                 payload = {
                     "operation_type": operation_type,
@@ -105,8 +106,7 @@ class APIAuthService(metaclass=MetaAPI):
                     "operator": current_identity["username"],
                 }
                 headers = request.headers
-                response = requests.put(
-                    ConfigClass.AUTH_SERVICE + "user/account", json=payload, headers=headers)
+                response = requests.put(ConfigClass.AUTH_SERVICE + "user/account", json=payload, headers=headers)
 
                 # send email
                 payload = {"email": user_email}
@@ -115,7 +115,7 @@ class APIAuthService(metaclass=MetaAPI):
                 if operation_type == "enable":
                     subject = "User enabled"
                     email_sender = SrvEmail()
-                    email_result = email_sender.send(
+                    email_sender.send(
                         subject,
                         [user_email],
                         msg_type="html",
@@ -130,14 +130,17 @@ class APIAuthService(metaclass=MetaAPI):
 
                     # check if platform admin
                     if user_info['role'] == 'admin':
-                        _logger.info("User status is changed to enabled , hence creating namespace folder for: %s"%(user_info.get("username")))
+                        _logger.info(
+                            "User status is changed to enabled , hence creating namespace folder for: %s"
+                            % (user_info.get("username"))
+                        )
                         # create namespace folder for all platform admin  once enabled
                         self.create_usernamespace_folder_admin(username=user_info.get("username"))
 
                 elif operation_type == "disable":
                     subject = "User disabled"
                     email_sender = SrvEmail()
-                    email_result = email_sender.send(
+                    email_sender.send(
                         subject,
                         [user_email],
                         msg_type="html",
@@ -154,39 +157,41 @@ class APIAuthService(metaclass=MetaAPI):
                 return {'result': "Error calling user account management service" + str(e)}, 500
 
         def create_usernamespace_folder_admin(self, username):
-            try:
-                url = ConfigClass.NEO4J_SERVICE + 'nodes/Container/query'
-                res = requests.post(url, json={})
-                if res.status_code == 200 and len(res.json()) > 0:
-                    for project in res.json():
-                        project_code = project["code"]
-                        self.create_folder(folder_name=username, project_code=project_code)
-            except Exception as error:
-                _logger.error(f"Error while querying Container details : {error}")
+            project_client = ProjectClientSync(ConfigClass.PROJECT_SERVICE, ConfigClass.REDIS_URL)
+            page = 0
+            result = project_client.search(page=page, page_size=50)
+            projects = result["result"]
+            while projects:
+                result = project_client.search(page=page, page_size=50)
+                projects = result["result"]
+                project_codes = [i.code for i in projects]
+                page += 1
+                self.bulk_create_folder_usernamespace(username, project_codes)
 
-        def create_folder(self, folder_name, project_code):
+        def bulk_create_folder_usernamespace(self, username: str, project_codes: list):
             try:
-                _logger.info(
-                    f"creating namespace folder in greenroom and core for user : {folder_name} under {project_code}")
                 zone_list = ["greenroom", "core"]
+                folders = []
                 for zone in zone_list:
-                    payload = {
-                        "folder_name": folder_name,
-                        "project_code": project_code,
-                        "zone": zone,
-                        "uploader": folder_name,
-                        "tags": []
-                    }
-                    folder_creation_url = ConfigClass.DATA_UPLOAD_SERVICE_GREENROOM + '/folder'
-                    res = requests.post(
-                        url=folder_creation_url,
-                        json=payload
-                    )
-                    if res.status_code == 200:
-                        _logger.info(
-                            f"Namespace folder created successfully for user : {folder_name} under {project_code}")
-                    elif res.status_code == 409:
-                        _logger.info(f"Namespace folder already exists for user : {folder_name} under {project_code}")
-            except Exception as error:
-                _logger.error(
-                    f"Error while trying to create namespace folder for user : {folder_name} under {project_code} : {error}")
+                    for project_code in project_codes:
+                        folders.append({
+                            "name": username,
+                            "zone": 0 if zone == "greenroom" else 1,
+                            "type": "name_folder",
+                            "owner": username,
+                            "container_code": project_code,
+                            "container_type": "project",
+                            "size": 0,
+                            "location_uri": "",
+                            "version": "",
+                        })
+
+                payload = {"items": folders, "skip_duplicates": True}
+                res = requests.post(ConfigClass.METADATA_SERVICE + 'items/batch/', json=payload)
+                if res.status_code != 200:
+                    raise APIException(status_code=EAPIResponseCode.internal_error.value, error_msg=res.json())
+
+            except Exception as e:
+                error_msg = f"Error while trying to bulk create namespace folder, error: {e}"
+                _logger.error(error_msg)
+                raise APIException(status_code=EAPIResponseCode.internal_error.value, error_msg=error_msg)
