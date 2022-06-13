281c925949c48f9ecb6a5e3223db3ee467f0024d
mammolia <amammoliti@indocresearch.org>
2022-06-13 16:32:17 -0400

    fix manifest zone permissions

--- api/api_data_manifest/utils.py
+++ api/api_data_manifest/utils.py
@@ -6,7 +6,7 @@ from services.permissions_service.utils import get_project_role
 
 
 def is_greenroom(file_node):
-    if file_node["zone"] == 0:
+    if file_node["zone"] == 1:
         return False
     else:
         return True
@@ -24,15 +24,14 @@ def has_permissions(template_id, file_node):
 
     if current_identity["role"] != "admin":
         role = get_project_role(manifest['project_code'])
-        if role != "admin":
-            if role == "contributor":
-                # contrib must own the file to attach manifests
+        if role == "contributor":
+            # contrib must own the file to attach manifests
+            root_folder = file_node["parent_path"].split(".")[0]
+            if root_folder != current_identity["username"]:
+                return False
+        elif role == "collaborator":
+            if is_greenroom(file_node):
                 root_folder = file_node["parent_path"].split(".")[0]
                 if root_folder != current_identity["username"]:
                     return False
-            elif role == "collaborator":
-                if is_greenroom(file_node):
-                    root_folder = file_node["parent_path"].split(".")[0]
-                    if root_folder != current_identity["username"]:
-                        return False
     return True
